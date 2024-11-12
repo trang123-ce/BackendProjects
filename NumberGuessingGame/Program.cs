fdfe4c9e05a6ce5532ed6ab8ffc48873aad69c2f
trang123-ce <64114601+trang123-ce@users.noreply.github.com>
2024-11-12 23:41:44 +0700

    Add files via upload
    It is a CLI-based game, so you need to use the command line to interact with the game. The game should work as follows:
    
    When the game starts, it should display a welcome message along with the rules of the game.
    The computer should randomly select a number between 1 and 100.
    User should select the difficulty level (easy, medium, hard) which will determine the number of chances they get to guess the number.
    The user should be able to enter their guess.
    If the user’s guess is correct, the game should display a congratulatory message along with the number of attempts it took to guess the number.
    If the user’s guess is incorrect, the game should display a message indicating whether the number is greater or less than the user’s guess.
    The game should end when the user guesses the correct number or runs out of chances.

---
+++ NumberGuessingGame/Program.cs
@@ -0,0 +1,61 @@
+﻿Console.WriteLine("Welcome to the Number Guessing Game!\r\nI'm thinking of a number between 1 and 100.\r\nYou have 5 chances to guess the correct number.\r\n\r\nPlease select the difficulty level:\r\n1. Easy (10 chances)\r\n2. Medium (5 chances)\r\n3. Hard (3 chances)");
+
+
+Console.WriteLine("Enter your choice: ");
+string choice = Console.ReadLine();
+
+int total_chance;
+
+if (choice == "1")
+{
+    Console.WriteLine("Great! You have selected the Easy level.\r\nLet's start the game!\r\n");
+    total_chance = 10;
+}
+else if (choice == "2")
+{
+    Console.WriteLine("Great! You have selected the Medium Hard level.\r\nLet's start the game!\r\n");
+    total_chance = 5;
+}
+else
+{
+    Console.WriteLine("Great! You have selected the Hard level.\r\nLet's start the game!\r\n");
+    total_chance = 3;
+}
+
+int guess, x, flag = 0;
+
+Random random = new Random();
+guess = random.Next(1, 101);
+Console.WriteLine(guess);
+
+for (int i = 0; i < total_chance; i++)
+{
+    Console.WriteLine("Enter your guess: ");
+    x = Convert.ToInt16(Console.ReadLine());
+    if (x == guess)
+    {
+        Console.WriteLine("Congratulations! You guessed the correct number in " + i + " attempts.");
+        flag = 1;
+        return;
+    }
+    else
+    {
+        if (x > guess)
+        {
+            Console.WriteLine("Incorrect! The number is less than " + x + ".");
+            int chance_left = total_chance - (i + 1);
+            Console.WriteLine("You have " + chance_left + " turns left!");
+        }
+        else
+        {
+            Console.WriteLine("Incorrect! The number is greater than " + x + ".");
+            int chance_left = total_chance - (i + 1);
+            Console.WriteLine("You have " + chance_left + " turns left!");
+        }
+    }
+}
+
+if (flag == 0)
+{
+    Console.WriteLine("The number is " + guess + ". Better luck next time!");
+}
